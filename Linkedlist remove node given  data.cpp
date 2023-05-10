abe80f77f9bb15184b9e454ee837916174da8755
arka2752 <arka25253@gmail.com>
2023-05-11 00:27:39 +0600

    add two

---
+++ Linkedlist remove node given  data.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *HEAD = NULL;
+
+Node *create_node(int item)
+{
+    Node *new_node = new Node;
+    if (new_node == NULL)
+    {
+        cout << "OVERFLOW" << endl;
+        exit(1);
+    }
+    new_node->data = item;
+    new_node->next = NULL;
+}
+void insert_at_end(Node *addnode)
+{
+    if (HEAD == NULL)
+    {
+        HEAD = addnode;
+    }
+    else
+    {
+        Node *ptr = HEAD;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = addnode;
+    }
+}
+
+void insert_at_beg(Node *addnode)
+{
+    addnode->next = HEAD;
+    HEAD = addnode;
+}
+
+void remove_by_given_item(int item)
+{
+    Node *ptr = HEAD;
+    Node *save;
+    while (ptr->data != item)
+    {
+        save = ptr;
+        ptr = ptr->next;
+        // cout << save->data << endl;
+    }
+    // cout << "----" << endl;
+    // cout << save->data << endl;
+    // cout << ptr->data << endl;
+    save->next = ptr->next;
+}
+
+void print()
+{
+    Node *ptr = HEAD;
+    while (ptr != NULL)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+}
+int main()
+{
+    Node *first_node = create_node(5);
+    insert_at_end(first_node);
+    Node *second_node = create_node(8);
+    insert_at_end(second_node);
+    Node *third_node = create_node(7);
+    insert_at_end(third_node);
+
+    print();
+    cout << endl;
+    remove_by_given_item(8);
+    cout << "After remove = ";
+    print();
+}
